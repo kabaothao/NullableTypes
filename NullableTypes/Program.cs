ab17513562b69f4e257f6e930e6163f4f20877a4
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-12 17:12:34 -0500

    updated codes and comments

--- NullableTypes/Program.cs
+++ NullableTypes/Program.cs
@@ -9,13 +9,23 @@ namespace NullableTypes // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            DateTime? date = new DateTime(2014, 1, 1);
-            DateTime date2 = date.GetValueOrDefault();
+            DateTime? date = null;
+            DateTime date2 = date ?? DateTime.Today;
+
+            DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+
+
             Console.WriteLine(date2);
 
-            DateTime? date3 = date2;
 
-            Console.WriteLine(date3.GetValueOrDefault());
+
+            //DateTime? date = new DateTime(2014, 1, 1);
+            //DateTime date2 = date.GetValueOrDefault();
+            //Console.WriteLine(date2);
+
+            //DateTime? date3 = date2;
+
+            //Console.WriteLine(date3.GetValueOrDefault());
 
 
 
